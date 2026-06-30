27ab71de02992cd80dbf7d872489a631eb3faef9
Hurogrammer <kh6197@gmail.com>
2026-06-30 22:22:44 +0900

    Initial guidance server project

---
+++ src/main/java/esum/escm/guidance_server/GuidanceServerApplication.java
@@ -0,0 +1,13 @@
+package esum.escm.guidance_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuidanceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuidanceServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/esum/escm/guidance_server/GuidanceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package esum.escm.guidance_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuidanceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
